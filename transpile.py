49973ef4bee86bd3c418f8944c48494e32fe63d8
Tianshu Huang <thetianshuhuang@gmail.com>
2019-02-11 00:26:31 -0600

    Basic work on transpile

--- transpile.py
+++ transpile.py
@@ -1,91 +1,256 @@
 
-
+from print import *
 import os
+import sys
+import re
 
 
-# Assemble includes
-def get_includes(files):
+__HEADER = """
+     _                         _ _
+    | |_ _ _ __ _ _ _  ____ __(_) |___
+    |  _| '_/ _` | ' \(_-< '_ \ | / -_)
+     \__|_| \__,_|_||_/__/ .__/_|_\___|
+    Transpiler for C    |_|       v1.0
 
-    includes = []
-    for f in files:
-        with open(f) as file:
-            includes += [
-                line for line in file.readlines() if
-                line.startswith("#include <")]
-    return "".join(list(set(includes)))
+"""
+__AUTHOR = "Tianshu Huang"
+__HINT = """
+Usage: python transpile.py <target_file> <output_file> <header>
 
-
-def get_defines(files):
-    defines = []
-    for f in files:
-        with open(f) as file:
-            defines += [
-                line for line in file.readlines() if
-                line.startswith("#define")
-            ]
-    return "".join(defines)
-
-
-def strip_newlines(lines):
-    for idx, line in enumerate(lines):
-        if idx < len(lines) - 2:
-            if line == '\n' and lines[idx + 1] == '\n' and lines[idx + 2] == '\n':
-                lines[idx] = ''
+target_file:
+    Target file to transpile. The transpiler will follow #includes as
+    appropriate. If a .h file is included, the corresponding .c file will
+    be as well (if it exists).
+output_file:
+    Target file to save to. If no output file is specified, the output will
+    be sent to stdout.
+header:
+    Header to place at the top of the document. This file should contain
+    any comment headers, as well as #defines that specify configuration
+    options. If no header is specified, __header__.c is used if present.
+"""
 
 
-def get(files):
+def get_file_includes(filename):
+    """Fetch includes from a C file (.c or .h)
 
-    lines = []
+    Parameters
+    ----------
+    filename : str
+        File to parse
 
-    for f in files:
-        if lines != []:
-            lines += ["\n\n/*" + "*" * 77 + "*/\n\n"]
-        with open(f) as file:
-            lines += [
-                line for line in file.readlines() if
-                not line.startswith("#")]
+    Returns
+    -------
+    [str[], str[]]
+        [libc includes, user includes]
+    """
 
-    strip_newlines(lines)
+    std_includes = []
+    user_includes = []
 
-    return "".join(lines)
+    with open(filename) as f:
+        includes = [
+            line for line in f.readlines()
+            if line.startswith("#include")]
+
+        for inc in includes:
+            std_includes += re.findall(r'<(.*)>', inc)
+            user_includes += re.findall(r'"(.*)"', inc)
+
+    return std_includes, user_includes
+
+
+def get_includes(base_file):
+    """Get includes referenced (recursively) by a base file
+
+    Parameters
+    ----------
+    base_file : str
+        File to follow links from
+
+    Returns
+    -------
+    [str[], str[]]
+        [libc includes, user includes]
+    """
+
+    std_includes = []
+    user_includes = []
+    file_stack = [os.path.basename(base_file)]
+
+    base_dir = os.path.dirname(base_file)
+
+    while len(file_stack) > 0:
+
+        target = file_stack.pop()
+        std_add, user_add = get_file_includes(os.path.join(base_dir, target))
+
+        # Handle .c files
+        if os.path.isfile(os.path.join(base_dir, target[:-2] + '.c')):
+            std_add_c, user_add_c = get_file_includes(
+                os.path.join(base_dir, target[:-2] + '.c'))
+            std_add += std_add_c
+            user_add += user_add_c
+
+        std_includes = list(set(std_includes + std_add))
+        for f in user_add:
+            if f not in file_stack and f not in user_includes:
+                user_includes.append(f)
+                file_stack.append(f)
+
+    return std_includes, user_includes
+
+
+def make_includes(std_includes):
+    """Make #include section
+
+    Parameters
+    ----------
+    std_includes : str[]
+        List of clib includes
+
+    Returns
+    -------
+    str
+        clib includes in string form
+    """
+
+    # Skip section if no std includes
+    if len(std_includes) == 0:
+        return ""
+
+    out = "\n\n" + putil.join(
+        "/* \n * \n * \n * \n */ ",
+        putil.clear_fmt(render("libc includes", SM))) + '\n\n'
+
+    for inc in std_includes:
+        out += '#include <{s}>\n'.format(s=inc)
+
+    return out
+
+
+def get_contents(filelist):
+    """Get contents of a list of files, and transpile into a single string
+
+    Parameters
+    ----------
+    filelist : str[]
+        List of files to transpile
+
+    Returns
+    -------
+    str
+        code transpiled into a single string
+    """
+
+    content = ""
+
+    for filename in filelist:
+        if os.path.isfile(filename):
+
+            if content != "":
+                content += "\n\n/*" + "*" * 77 + "*/\n\n"
+
+            with open(filename) as file:
+                lines = [
+                    i for i in file.readlines()
+                    if not i.startswith("#include")
+                ]
+                content += "".join(lines)
+        else:
+            print(
+                "// [Warning] File {name} does not exist"
+                .format(name=filename), BR + YELLOW)
+
+    return content
+
+
+def make_header(base_dir, user_includes):
+    """Compile header section of code
+
+    Parameters
+    ----------
+    base_dir : str
+        Base directory
+    user_includes : str[]
+        List of user files to include in the header
+
+    Returns
+    -------
+    str
+        Header files transpiled into a single string
+    """
+
+    header = get_contents([
+        os.path.join(base_dir, h_name) for h_name in user_includes])
+
+    return (
+        "\n\n" +
+        putil.join(
+            "/* \n * \n * \n * \n */ ",
+            putil.clear_fmt(render("user headers", SM))) +
+        "\n" + header)
+
+
+def make_c(base_dir, user_includes):
+    """Compile main (.c) section of code
+
+    Parameters
+    ----------
+    base_dir : str
+        Base directory
+    user_includes : str[]
+        List of user files to get corresponding .c files for
+
+    Returns
+    -------
+    str
+        Code files transpiled into a single string
+    """
+
+    header = get_contents([
+        os.path.join(base_dir, h_name[:-2] + '.c')
+        for h_name in user_includes])
+
+    return (
+        "\n\n" +
+        putil.join(
+            "/* \n * \n * \n * \n */ ",
+            putil.clear_fmt(render("c code", SM))) +
+        "\n" + header)
 
 
 if __name__ == "__main__":
-    files = [
-        f for f in os.listdir(os.getcwd())
-        if os.path.isfile(os.path.join(os.getcwd(), f))]
-    c_files = [f for f in files if len(f) > 2 and f[-2:] == '.c']
-    h_files = [f for f in files if len(f) > 2 and f[-2:] == '.h']
-    files = c_files + h_files
 
-    print(r"""/**
- *  ___ _  _  ___ _   _   _ ___  ___ ___
- * |_ _| \| |/ __| | | | | |   \| __/ __|
- *  | || .` | (__| |_| |_| | |) | _|\__ \
- * |___|_|\_|\___|____\___/|___/|___|___/
- */""")
-    print(get_includes(files))
+    print(__HEADER, RED, BOLD)
 
-    print(r"""/**
- *  ___  ___ ___ ___ _  _ ___ ___
- * |   \| __| __|_ _| \| | __/ __|
- * | |) | _|| _| | || .` | _|\__ \
- * |___/|___|_| |___|_|\_|___|___/
- */""")
-    print(get_defines(files))
+    if len(sys.argv) < 2:
+        print(__HINT)
+        exit()
 
-    print(r"""/**
- *  _  _ ___   _   ___  ___ ___  ___
- * | || | __| /_\ |   \| __| _ \/ __|
- * | __ | _| / _ \| |) | _||   /\__ \
- * |_||_|___/_/ \_\___/|___|_|_\|___/
- */""")
-    print(get(h_files))
+    target = sys.argv[1]
+    base_dir = os.path.dirname(target)
 
-    print(r"""/**
- *   ___    ___ ___  ___  ___
- *  / __|  / __/ _ \|   \| __|
- * | (__  | (_| (_) | |) | _|
- *  \___|  \___\___/|___/|___|
- */""")
-    print(get(c_files))
+    if len(sys.argv) >= 3:
+        output = open(sys.argv[2], "w")
+    else:
+        output = sys.stdout
+
+    std, usr = get_includes(target)
+
+    print("libc includes [{i}]:".format(i=len(std)))
+    print("    " + ", ".join(std))
+    print("User files [{i}]:".format(i=len(usr)))
+    print("    " + ", ".join(usr))
+
+    output.write(make_includes(std))
+    output.write(make_header(base_dir, usr))
+    output.write(make_c(base_dir, usr + [sys.argv[1]]))
+    print(
+        "Transpiled {i} files to {o}".format(
+            i=len(std) + len(usr),
+            o="sys.stdout" if len(sys.argv) < 3 else sys.argv[2]),
+        BR + GREEN, BOLD)
+
+    if len(sys.argv) >= 2:
+        output.close()
